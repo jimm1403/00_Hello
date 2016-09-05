66f90c29ee2d4e90b4dc986bff34921d4738a1e4
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-05 10:38:42 +0200

    Added Hello() method Added Hello(string _name) method

--- 00_Hello/Functions.cs
+++ 00_Hello/Functions.cs
@@ -1,6 +1,15 @@
 ﻿namespace _00_Hello
 {
     public class Functions
-    {  
+    {
+        public static string Hello()
+        {
+            return "hello";
+        }
+
+        public static string Hello(string _name)
+        {
+            return "hello, " + _name;
+        }
     }
 }
